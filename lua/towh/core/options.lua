23bc1f7f88df9e6dbcd1f5d5caf189f5bc070d64
towh2301 <huy.buihoang2301@gmail.com>
2023-06-13 00:58:56 +0700

    nvim-config

--- lua/towh/core/options.lua
+++ lua/towh/core/options.lua
@@ -24,7 +24,7 @@ opt.cursorline = true -- highlight the current cursor line
 
 -- turn on termguicolors for nightfly colorscheme to work
 -- (have to use iterm2(Mac only) or any other true color terminal)
-opt.termguicolors = false
+-- opt.termguicolors = false
 opt.background = "dark" -- colorschemes that can be light or dark will be made dark
 opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 
