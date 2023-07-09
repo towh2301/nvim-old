c2c33bdb10d4cd4a138c56677ddd14182f072f58
towh2301 <huy.buihoang2301@gmail.com>
2023-07-09 16:31:08 +0700

    update-dap

--- lua/towh/plugins/debug/debug.lua
+++ lua/towh/plugins/debug/debug.lua
@@ -25,7 +25,8 @@ require("mason-nvim-dap").setup({
   -- online, please don't ask me how to install them :)
   ensure_installed = {
     -- Update this to ensure that you have the debuggers for the langs you want
-    "delve",
+
+    -- "delve",
   },
 })
 
